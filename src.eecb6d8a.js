cbb4ac1fda4b86857b34c89b6c7239c8ac25a657
lidanko12 <lidanko12@gmail.com>
2021-07-27 16:28:46 +0000

    Deploying to gh-pages from @ lidanko12/goit-js-hw-11-color-switch@4a60fd35dc98c5bc8d57213bffc64dd2045c248b 🚀

--- src.eecb6d8a.js
+++ src.eecb6d8a.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.9139aab2.js.map
+},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"a4By":[function(require,module,exports) {
+const t=["#FFFFFF","#2196F3","#4CAF50","#FF9800","#009688","#795548"],e={startBtn:document.querySelector('button[data-action="start"]'),stopBtn:document.querySelector('button[data-action="stop"]'),body:document.body},o=(t,e)=>Math.floor(Math.random()*(e-t+1)+t),s={intervalID:null,isActive:!1,start(){this.isActive||(this.isActive=!0,this.colorsSwitch=setInterval(()=>{let s=o(0,t.length-1);e.body.style.backgroundColor=t[s]},1e3))},stop(){clearInterval(this.colorsSwitch),this.isActive=!1}};e.startBtn.addEventListener("click",s.start.bind(s)),e.stopBtn.addEventListener("click",s.stop.bind(s));
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/color-picker.js");
+},{"./sass/main.scss":"clu1","./js/color-picker.js":"a4By"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11-color-swi/src.eecb6d8a.js.map
